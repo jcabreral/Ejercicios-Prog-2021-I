459d3ab48099eaf7ceb5485248ec6eaf93fd5181
jcabreral <jcabreral@unal.edu.com>
2021-03-05 11:43:13 -0500

    Se agregan las funciones suma y multiplicación

--- var_4.cpp
+++ var_4.cpp
@@ -1,13 +1,33 @@
 #include<iostream>
 
+void suma(float numa, float numb);
+void multip(float numa, float numb);
+
 int main(void){
 
-    int val1;
-    std::cout<<"Ingrese un valor: ";
+    float  val1, val2;
+    std::cout<<"Ingrese el primer  valor: ";
     std::cin>>val1;
+    std::cout<<"Ingrese el segundo  valor: ";
+    std::cin>>val2;
 
-    std::cout<<"El valor ingresado es: "<<val1<<"\n";
+
+    suma(val1, val2);
+    multip(val1, val2);
 
 
     return 0;
+}
+
+void suma(float numa, float numb){
+
+    float result;
+    result = numa + numb;
+    std::cout<<"El resultado de la suma es: "<<result<<"\n";
+}
+
+void multip(float numa, float numb){
+    float result;
+    result = numa * numb;
+    std::cout<<"El resultado de la multiplicación es: "<<result<<"\n";
 }
