a7ad2f611618ddafb31da4b691c905e52b3aeccc
jcabreral <jcabreral@unal.edu.com>
2021-03-05 03:21:22 -0500

    Ejercicio 4

---
+++ var_4.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+int main(void){
+
+    int val1;
+    std::cout<<"Ingrese un valor: ";
+    std::cin>>val1;
+
+    std::cout<<"El valor ingresado es: "<<val1<<"\n";
+
+
+    return 0;
+}
