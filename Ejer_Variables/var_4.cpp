73c45e82d3c8cbf785c3d7f617cca2f6dfb3fed1
jcabreral <jcabreral@unal.edu.com>
2021-03-12 22:17:14 -0500

    Se completa el ejercicio 4

--- Ejer_Variables/var_4.cpp
+++ Ejer_Variables/var_4.cpp
@@ -1,7 +1,9 @@
 #include<iostream>
 
 void suma(float numa, float numb);
+void resta(float numa, float numb);
 void multip(float numa, float numb);
+void mayor(float numa, float numb);
 
 int main(void){
 
@@ -13,8 +15,9 @@ int main(void){
 
 
     suma(val1, val2);
+    resta(val1, val2);
     multip(val1, val2);
-
+    mayor(val1, val2);
 
     return 0;
 }
@@ -26,8 +29,23 @@ void suma(float numa, float numb){
     std::cout<<"El resultado de la suma es: "<<result<<"\n";
 }
 
+void resta(float numa, float numb){
+    float result;
+    result = numa - numb;
+    std::cout<<"El resultado de la resta de "<<numa<<"-"<<numb<<" es: "<<result<<"\n";
+}
+
 void multip(float numa, float numb){
     float result;
     result = numa * numb;
     std::cout<<"El resultado de la multiplicación es: "<<result<<"\n";
+}
+
+void mayor(float numa, float numb){
+    if(numa > numb){
+        std::cout<<numa<<" es mayor que "<<numb<<"\n";
+    }
+    else{
+        std::cout<<numb<<" es mayor que "<<numa<<"\n";
+    }
 }
