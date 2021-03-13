63db1b433a043ed2e27456ad70100df54c06491d
jcabreral <jcabreral@unal.edu.com>
2021-03-12 22:42:04 -0500

    Ejercicio 6 variables, falta la función

---
+++ Ejer_Variables/var_6.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+void organizar(int numa, int numb, int numc);
+
+int main(int argc, char **argv){
+
+    int val1, val2, val3;
+
+    std::cout<<"Ingrese el primer valor: ";
+    std::cin>>val1;
+    std::cout<<"Ingrese el segundo valor: ";
+    std::cin>>val2;
+    std::cout<<"Ingrese el tercer valor: ";
+    std::cin>>val3;
+
+    organizar(val1, val2, val3);
+
+    return 0;
+}
+
+void organizar(int numa, int numb, int numc){
+
+    std::cout<<"Hay que completar la función\n";
+
+}
